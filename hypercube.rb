8c32935bb1618e85db257c2de8ee4d2f7acc5135
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-28 00:35:33 +0900

    add Hypercube class

--- hypercube.rb
+++ hypercube.rb
@@ -1,5 +1,25 @@
-class Hypercube < Graph
-  def initialize
-    puts "Hypercube Init"
+class Hypercube
+  attr_reader   :dim, :size
+  attr_accessor :neighbor
+
+
+  def initialize(dim)
+    @dim      = dim
+    @size     = 2**dim
+    @neighbor = Array.new
+  end
+
+  def get_neighbor
+    neighbor = Array.new
+    for address in 0...size
+      for i in 0...dim
+        neighbor[address] ||= Array.new
+        neighbor[address].push address^(2**i)
+      end
+    end
+    neighbor
   end
 end
+
+hypercube = Hypercube.new(4)
+p hypercube.get_neighbor
