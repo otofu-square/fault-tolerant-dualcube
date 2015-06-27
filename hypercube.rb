bb3a34e2f0c6746be8b6efa9b52a08068ef04201
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-28 00:11:26 +0900

    delete Graph class

--- hypercube.rb
+++ hypercube.rb
@@ -1,14 +1,5 @@
-class Graph
-  def initialize
-    puts "Graph Init"
-  end
-end
-
 class Hypercube < Graph
   def initialize
     puts "Hypercube Init"
   end
 end
-
-p graph = Graph.new
-p hc = Hypercube.new
