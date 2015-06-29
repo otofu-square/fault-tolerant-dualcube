32beb140d660abe7a643d75f2614c36c8f269026
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-29 14:43:19 +0900

    define #get_distance & #count_bit

--- hypercube.rb
+++ hypercube.rb
@@ -8,6 +8,10 @@ class Hypercube
     @fault    = set_fault(ratio)
   end
 
+  def get_distance(s, d)
+    count_bit(s^d)
+  end
+
   def print_nodes
     for address in 0...size
       puts "%4d : #{get_binary_address(address)} [f=#{fault[address]}]" % address
@@ -32,10 +36,20 @@ class Hypercube
     fault
   end
 
+  def count_bit(i)
+    i = i - ((i >> 1) & 0x55555555)
+    i = (i & 0x33333333) + ((i >> 2) & 0x33333333)
+    i = (i + (i >> 4)) & 0x0f0f0f0f
+    i = i + (i >> 8)
+    i = i + (i >> 16)
+    i & 0x3f;
+  end
+
   def get_binary_address(address)
     "%0#{dim}b" % address
   end
 end
 
-hypercube = Hypercube.new(20, 0.5)
+hypercube = Hypercube.new(10, 0.5)
 hypercube.print_nodes
+p hypercube.get_distance(0, 1023)
