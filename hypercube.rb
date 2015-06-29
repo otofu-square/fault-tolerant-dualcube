fb53d91297e157052450072176fdad49e0dc429c
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-29 16:07:02 +0900

    refactor hypercube.rb

--- hypercube.rb
+++ hypercube.rb
@@ -1,23 +1,24 @@
 require './bit_counter'
+require './node_printer'
 
 class Hypercube
+  include NodePrinter
+
   attr_reader :dim, :size, :neighbors, :fault
 
   def initialize(dim, ratio=0.0)
-    @dim      = dim
-    @size     = 2**dim
-    @neighbor = set_neighbors
-    @fault    = set_fault(ratio)
+    @dim       = dim
+    @size      = 2**dim
+    @neighbors = set_neighbors
+    @fault     = set_fault(ratio)
   end
 
-  def get_distance(s, d)
-    Integer::count_bit(s^d)
+  def neighbor?(a, b)
+    neighbors[a].include?(b)
   end
 
-  def print_nodes
-    for address in 0...size
-      puts "%4d : #{get_binary_address(address)} [f=#{fault[address]}]" % address
-    end
+  def get_distance(a, b)
+    Integer::count_bit(a^b)
   end
 
   private
@@ -37,12 +38,8 @@ class Hypercube
     (0...size).to_a.sample((size*ratio).floor).each { |i| fault[i] = 1 }
     fault
   end
-
-  def get_binary_address(address)
-    "%0#{dim}b" % address
-  end
 end
 
 hypercube = Hypercube.new(10, 0.5)
 hypercube.print_nodes
-p hypercube.get_distance(0, 1023)
+p hypercube.neighbor?(0, 3)
